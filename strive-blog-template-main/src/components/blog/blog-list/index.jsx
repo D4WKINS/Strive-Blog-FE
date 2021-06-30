9b45dc7c99b092953d28d3733f49f7b1bfa0c33b
Carl <78683271+D4WKINS@users.noreply.github.com>
2021-06-30 12:46:28 +0000

    adds start script with serve

--- strive-blog-template-main/src/components/blog/blog-list/index.jsx
+++ strive-blog-template-main/src/components/blog/blog-list/index.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
-import posts from "../../../data/posts.json";
+// import posts from "../../../data/posts.json";
 export default class BlogList extends Component {
+
+  
+  state ={
+    posts:[]
+  }
+
+  async fetchPosts(){
+    try{
+    let response = await fetch("http://localhost:3001/blogs")
+    if(response.ok){
+    let allPosts = await response.json()
+    this.setState({posts:allPosts})
+    console.log(this.state.posts)
+
+    }
+  }catch(err){
+    console.log("Something went wrong",err)
+  }
+  }
+   componentDidMount=()=>{
+      this.fetchPosts()
+   }
+
   render() {
     return (
       <Row>
-        {posts.map((post) => (
+        {this.state.posts.map((post) => (
           <Col md={4} style={{ marginBottom: 50 }}>
             <BlogItem key={post.title} {...post} />
           </Col>
